80a6add855c88649b1b2b5545f87829e64b09dbb
Nicolas Laval <nicolas.laval@insee.fr>
2018-08-06 17:39:30 +0200

    Add sdmx objects

--- src/js/rdf/naming.js
+++ src/js/rdf/naming.js
@@ -1,5 +1,8 @@
 export const CONCEPTS_GRAPH = 'http://rdf.insee.fr/graphs/concepts';
 export const CODES_GRAPH = 'http://rdf.insee.fr/graphs/codes';
+export const DIMENSIONS_GRAPH = 'http://rdf.insee.fr/graphs/dimensions';
+export const MEASURES_GRAPH = 'http://rdf.insee.fr/graphs/measures';
+export const ATTRIBUTS_GRAPH = 'http://rdf.insee.fr/graphs/attributs';
 
 const BASE_DSD_GRAPH = 'http://rdf.insee.fr/graphs/dsd/';
 export const setDSDGraph = id => `${BASE_DSD_GRAPH}${id}`;
--- src/js/rdf/store/init-store.js
+++ src/js/rdf/store/init-store.js
@@ -1,11 +1,14 @@
-import { CONCEPTS_GRAPH, CODES_GRAPH, setDSDGraph } from 'js/rdf/naming';
+import * as N from 'js/rdf/naming';
 
 const BASE_URL = window.location.origin;
 
 const inputInit = [
-	{ fileURL: `${BASE_URL}/concepts.ttl`, graph: CONCEPTS_GRAPH },
-	{ fileURL: `${BASE_URL}/codes.ttl`, graph: CODES_GRAPH },
-	{ fileURL: `${BASE_URL}/dsd-esane.ttl`, graph: setDSDGraph('esane') },
+	{ fileURL: `${BASE_URL}/concepts.ttl`, graph: N.CONCEPTS_GRAPH },
+	{ fileURL: `${BASE_URL}/codes.ttl`, graph: N.CODES_GRAPH },
+	{ fileURL: `${BASE_URL}/dimensions.ttl`, graph: N.DIMENSIONS_GRAPH },
+	{ fileURL: `${BASE_URL}/measures.ttl`, graph: N.MEASURES_GRAPH },
+	{ fileURL: `${BASE_URL}/attributs.ttl`, graph: N.ATTRIBUTS_GRAPH },
+	{ fileURL: `${BASE_URL}/dsd-esane.ttl`, graph: N.setDSDGraph('esane') },
 ];
 
 export const init = () =>
