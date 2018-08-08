9be60b1cac33bef6f088bd1b646e360474e72689
Nicolas Laval <nicolas.laval@insee.fr>
2018-08-08 17:05:59 +0200

    Update DSD edition

--- src/js/components/dsd/edition/controls.js
+++ src/js/components/dsd/edition/controls.js
@@ -3,19 +3,16 @@ import Button from 'js/components/shared/button';
 import D from 'js/i18n';
 import { cleanId } from 'js/utils/string-utils';
 
-const check = dsd =>
-	Object.values(dsd).reduce((_, v) => {
-		if (_ && v) return true;
-		return false;
-	}, true);
+const check = obj =>
+	Object.values(obj).reduce((_, v) => _ + v, '').length !== 0;
 
-export default ({ creation, save, dsd }) => (
+export default ({ creation, id, save, helpMsgObj }) => (
 	<div className="row btn-line">
 		<Button
 			label={D.btnReturn}
-			action={creation ? '/' : `/dsd/${cleanId(dsd.id)}`}
+			action={creation ? '/' : `/dsd/${cleanId(id)}`}
 		/>
 		<div className="col-md-8" />
-		<Button label={D.btnSave} action={save} disabled={!check(dsd)} />
+		<Button label={D.btnSave} action={save} disabled={check(helpMsgObj)} />
 	</div>
 );
--- src/js/components/dsd/edition/edition.js
+++ src/js/components/dsd/edition/edition.js
@@ -3,41 +3,84 @@ import PageTitle from 'js/components/shared/page-title';
 import Input from 'js/components/shared/input';
 import Controls from './controls';
 import D from 'js/i18n';
+import { dsdEditionMessage } from 'js/utils/edition-utils';
 
 class Edition extends Component {
 	constructor(props) {
 		super();
-		const defaultState = { id: '', label: '', description: '' };
+		const defaultState = {
+			id: '',
+			labelFr: '',
+			labelEn: '',
+			descriptionFr: '',
+			descriptionEn: '',
+		};
 		this.state = Object.assign(defaultState, props.data);
 		this.onChange = (field, value) => this.setState({ [field]: value });
 		this.save = () => this.props.save(this.state);
 	}
 	render() {
 		const { title, creation } = this.props;
-		const { id, label, description } = this.state;
+		const { id, labelFr, labelEn, descriptionFr, descriptionEn } = this.state;
+		const helpMsgObj = dsdEditionMessage(id, labelFr, creation);
 		return (
 			<React.Fragment>
 				<PageTitle title={title} />
-				<Controls creation={creation} dsd={this.state} save={this.save} />
+				<Controls
+					creation={creation}
+					id={id}
+					save={this.save}
+					helpMsgObj={helpMsgObj}
+				/>
 				<Input
 					id="id"
 					label={D.idTitle}
 					value={id}
 					onChange={e => this.onChange('id', e.target.value)}
 					disabled={!creation}
+					helpMsg={helpMsgObj.id}
 				/>
-				<Input
-					id="label"
-					label={D.labelTitle}
-					value={label}
-					onChange={e => this.onChange('label', e.target.value)}
-				/>
-				<Input
-					id="description"
-					label={D.descriptionTitle}
-					value={description}
-					onChange={e => this.onChange('description', e.target.value)}
-				/>
+				<div className="row">
+					<div className="col-md-6">
+						<Input
+							id="labelFr"
+							label={D.labelTitle}
+							value={labelFr}
+							onChange={e => this.onChange('labelFr', e.target.value)}
+							lang="fr"
+							helpMsg={helpMsgObj.labelFr}
+						/>
+					</div>
+					<div className="col-md-6">
+						<Input
+							id="labelEn"
+							label={D.labelTitle}
+							value={labelEn}
+							onChange={e => this.onChange('labelEn', e.target.value)}
+							lang="en"
+						/>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-md-6">
+						<Input
+							id="descriptionFr"
+							label={D.descriptionTitle}
+							value={descriptionFr}
+							onChange={e => this.onChange('descriptionFr', e.target.value)}
+							lang="fr"
+						/>
+					</div>
+					<div className="col-md-6">
+						<Input
+							id="descriptionEn"
+							label={D.descriptionTitle}
+							value={descriptionEn}
+							onChange={e => this.onChange('descriptionEn', e.target.value)}
+							lang="en"
+						/>
+					</div>
+				</div>
 			</React.Fragment>
 		);
 	}
