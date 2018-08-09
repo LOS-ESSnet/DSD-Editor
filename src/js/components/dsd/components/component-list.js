85d92880ac2c5fdb64382ccf8999f5be7e8c290f
Nicolas Laval <nicolas.laval@insee.fr>
2018-08-09 11:12:15 +0200

    Add search field to component list

--- src/js/components/dsd/components/component-list.js
+++ src/js/components/dsd/components/component-list.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Input from 'js/components/shared/input';
 import Pagination from 'js/components/shared/pagination';
 import D from 'js/i18n';
-import { sortArray } from 'js/utils/array-utils';
+import { sortArray, filterDeburr } from 'js/utils/array-utils';
 
 const sortArrayByLabelFr = sortArray('labelFr');
 
-export default ({ checked, components, onChange }) => {
-	const items = sortArrayByLabelFr(
-		buildComponents(checked, components, onChange)
-	).map(a => a.item);
-	return <Pagination itemEls={items} itemsPerPage="10" />;
-};
+export default class ComponentList extends Component {
+	constructor() {
+		super();
+		this.state = { search: '' };
+		this.onChange = search => this.setState({ search });
+	}
+	render() {
+		const { checked, components, onChange } = this.props;
+		const { search } = this.state;
+		console.log(components);
+		const items = sortArrayByLabelFr(
+			buildComponents(checked, components, onChange, search)
+		).map(a => a.item);
+		return (
+			<React.Fragment>
+				<Input
+					value={search}
+					onChange={e => this.onChange(e.target.value)}
+					placeholder={D.searchLabelPlaceholder}
+				/>
+				<Pagination itemEls={items} itemsPerPage="10" />
+			</React.Fragment>
+		);
+	}
+}
 
 const getPillTitle = field => {
 	switch (field) {
@@ -25,10 +45,11 @@ const getPillTitle = field => {
 	}
 };
 
-const buildComponents = (checked, components, onChange) =>
+const buildComponents = (checked, components, onChange, search) =>
 	Object.keys(components).reduce((_, field) => {
 		if (checked[field])
 			components[field].forEach(({ URI, labelFr }, i) => {
+				if (!filterDeburr(search)(labelFr)) return _;
 				const type = (
 					<span className={`badge badge-pill badge-${field}`}>
 						{getPillTitle(field)}
