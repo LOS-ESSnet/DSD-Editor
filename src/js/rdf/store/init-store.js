7e93e80b2be4ecc8fe3ba2fd78446ea881eed89e
Nicolas Laval <nicolas.laval@insee.fr>
2018-08-14 11:39:25 +0200

    Update init resources

--- src/js/rdf/store/init-store.js
+++ src/js/rdf/store/init-store.js
@@ -5,9 +5,6 @@ const BASE_URL = window.location.origin;
 const inputInit = [
 	{ fileURL: `${BASE_URL}/concepts.ttl`, graph: N.CONCEPTS_GRAPH },
 	{ fileURL: `${BASE_URL}/codes.ttl`, graph: N.CODES_GRAPH },
-	{ fileURL: `${BASE_URL}/dimensions.ttl`, graph: N.DIMENSIONS_GRAPH },
-	{ fileURL: `${BASE_URL}/measures.ttl`, graph: N.MEASURES_GRAPH },
-	{ fileURL: `${BASE_URL}/attributs.ttl`, graph: N.ATTRIBUTS_GRAPH },
 	{ fileURL: `${BASE_URL}/pop5.ttl`, graph: N.setDSDGraph('pop5-2015-comarm') },
 ];
 
