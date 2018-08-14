9a85faca70b575e07e8442ca0c04317a3a094697
Nicolas Laval <nicolas.laval@insee.fr>
2018-08-14 14:48:10 +0200

    Disable pop5

--- src/js/rdf/store/init-store.js
+++ src/js/rdf/store/init-store.js
@@ -5,7 +5,7 @@ const BASE_URL = window.location.origin;
 const inputInit = [
 	{ fileURL: `${BASE_URL}/concepts.ttl`, graph: N.CONCEPTS_GRAPH },
 	{ fileURL: `${BASE_URL}/codes.ttl`, graph: N.CODES_GRAPH },
-	{ fileURL: `${BASE_URL}/pop5.ttl`, graph: N.setDSDGraph('pop5-2015-comarm') },
+	//{ fileURL: `${BASE_URL}/pop5.ttl`, graph: N.setDSDGraph('pop5-2015-comarm') },
 	{ fileURL: `${BASE_URL}/my-dsd.ttl`, graph: N.setDSDGraph('my-dsd') },
 ];
 
