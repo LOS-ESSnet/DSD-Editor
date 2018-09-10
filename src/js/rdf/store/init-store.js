8d9a7114fc2e41d50443d2ae74beb9d4bc67df81
Nicolas Laval <nicolas.laval@insee.fr>
2018-09-10 18:55:34 +0200

    Fix initial loading

--- src/js/rdf/store/init-store.js
+++ src/js/rdf/store/init-store.js
@@ -3,10 +3,19 @@ import * as N from 'js/rdf/naming';
 const BASE_URL = window.location.origin;
 
 const inputInit = [
-	{ fileURL: `${BASE_URL}/concepts.ttl`, graph: N.CONCEPTS_GRAPH },
-	{ fileURL: `${BASE_URL}/codes.ttl`, graph: N.CODES_GRAPH },
-	//{ fileURL: `${BASE_URL}/pop5.ttl`, graph: N.setDSDGraph('pop5-2015-comarm') },
-	{ fileURL: `${BASE_URL}/my-dsd.ttl`, graph: N.setDSDGraph('my-dsd') },
+  {
+    fileURL: `${BASE_URL || process.env.PUBLIC_URL}/concepts.ttl`,
+    graph: N.CONCEPTS_GRAPH
+  },
+  {
+    fileURL: `${BASE_URL || process.env.PUBLIC_URL}/codes.ttl`,
+    graph: N.CODES_GRAPH
+  },
+  //{ fileURL || process.env.PUBLIC_URL: `${BASE_URL}/pop5.ttl`, graph: N.setDSDGraph('pop5-2015-comarm') },
+  {
+    fileURL: `${BASE_URL || process.env.PUBLIC_URL}/my-dsd.ttl`,
+    graph: N.setDSDGraph("my-dsd")
+  }
 ];
 
 export const init = () =>
